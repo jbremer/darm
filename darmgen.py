0ad2b0d1b1c3525173a73ca3cd55498374d12acd
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-02-15 02:16:26 +0100

    sort instruction names, other magic

--- darmgen.py
+++ darmgen.py
@@ -6,18 +6,18 @@ def instruction_names(arr):
     def name(x):
         return x.split('{')[0].split('<')[0].split()[0]
 
-    return list(set(name(x[0]) for x in arr))
+    return sorted(set(name(x[0]) for x in arr))
 
 
 def instruction_names_enum(arr):
     """Enumeration of all instruction names."""
-    text = ', '.join('I_%s' % x for x in arr)
+    text = ', '.join('I_%s' % x for x in instruction_names(arr))
     text = '\n    '.join(textwrap.wrap(text, 74))
     return 'typedef enum {\n    %s\n} armv7_instr_t;' % text
 
 
 def instruction_names_table(arr):
     """Table of strings of all instructions."""
-    text = ', '.join('"%s"' % x for x in arr)
+    text = ', '.join('"%s"' % x for x in instruction_names(arr))
     text = '\n    '.join(textwrap.wrap(text, 74))
     return 'const char *armv7_mnemonics[] = {\n    %s\n};' % text
