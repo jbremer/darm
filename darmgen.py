55977c29d99f59874af0f9b8ef5640c370835346
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-06-26 19:54:22 +0200

    do not rely on 0b-prefix support by compilers anymore

--- darmgen.py
+++ darmgen.py
@@ -461,6 +461,16 @@ if __name__ == '__main__':
     print('extern const char *darm_enctypes[%d];' % len(instr_types))
     print('extern const char *darm_registers[16];')
 
+    # define constants 0b0 up upto 0b11111111
+    for x in range(256):
+        print('#define %s %d' % (bin(x)[1:], x))
+
+    # define partial constants with leading zeroes, such as 0b0001
+    for x in range(2, 7):
+        for y in itertools.product('01', repeat=x):
+            num = ''.join(y)
+            print('#define b%s %d' % (num, int(num, 2)))
+
     print('#endif')
 
     #
